8fa4b1709344b4f5f1f21d25a362e4cb7807907c
Ausin Formica <austin.formica@gmail.com>
2015-02-23 14:56:30 -0500

    http party returning questions and answers

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -2,7 +2,11 @@ class QuizzesController < ApplicationController
 
   def home
     @quizzes = Quiz.all
-
+    @quiz = Quiz.last
+    # @res = HTTParty.get("http://jservice.io/api/random")
+    @res = HTTParty.get("http://jservice.io/api/clues")
+    i = 0
+    # @response = @res[0]["question"]
   end
 
   def show
--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,3 +1,11 @@
 class Quiz < ActiveRecord::Base
   has_many :questions
+
+
+  def get_question
+    # url = "http://api.giphy.com/v1/gifs/search?q=" + self.mood.name + "&api_key=dc6zaTOxFJmzC"
+    # response = HTTParty.get(url)
+    response = HTTParty.get("http://jservice.io/api/random")
+    return response[0]["question"]
+  end
 end
