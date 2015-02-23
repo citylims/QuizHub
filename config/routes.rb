7acb10d5439c97e032d8bce2891f9a9fda6d1d8c
Ausin Formica <austin.formica@gmail.com>
2015-02-23 10:29:09 -0500

    sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
  root 'quizzes#home'
+
+ match "/auth/:provider/callback" => "sessions#create", via: [:get]
+
+  match "/signout" => "sessions#destroy", :as => :signout, via: [:get]
+
 end
