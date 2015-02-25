bb8c4ca061272f0a3b7ebd496ba4b74acf4c4c83
Ausin Formica <austin.formica@gmail.com>
2015-02-24 22:04:59 -0500

    implement meter

--- app/assets/javascripts/quiz.js
+++ app/assets/javascripts/quiz.js
@@ -57,8 +57,60 @@ function checkAnswer(a,b) {
         }
     }
     var weight = equivalency / maxLength;
-    console.log((weight * 100) + "%");
+    // console.log((weight * 100) + "%");
+    weight = (weight * 100)
+    if (weight > 80) {
+      count++
+      var percentage = Math.ceil(100*count/total);
+      console.log(percentage);
+      filler -= (size/total)
+      console.log(filler);
+      runner(percentage, filler);
+      console.log(count);
+  }
 }
+
+var paths = document.getElementsByClassName('meter');
+var path = paths[paths.length-1];
+
+$(path).css({
+  'stroke-dasharray': 500,
+  'stroke-dashoffset': 0,
+  'opacity': 1,
+})
+
+function runner(percentage, fill){
+  setTimeout( function() {
+    $('.percentage').text(percentage)
+    $(path).css('stroke-dashoffset', fill);
+  }, (Math.random() * 200));
+}
+
+var size = 377;
+var total = 10;
+var count = 0;
+var filler = 0;
+
+
+var question = $("#side_one").val()
+var answer = $("#side_two").val();
+
+
+$("#side_one").on("click", function() {
+  if (count === total) {
+    console.log("complete");
+  }
+  else {
+    count++
+    var percentage = Math.ceil(100*count/total);
+    console.log(percentage);
+    filler -= (size/total)
+    console.log(filler);
+    runner(percentage, filler);
+    console.log(count);
+  }
+})
+
 
 
 
