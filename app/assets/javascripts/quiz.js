422607a987d83a7e301f9bf70548cacbc453b2dd
Ausin Formica <austin.formica@gmail.com>
2015-02-25 20:00:37 -0500

    interface refactor

--- app/assets/javascripts/quiz.js
+++ app/assets/javascripts/quiz.js
@@ -5,13 +5,11 @@ $('.go').on('click', function(){
   $('.go').hide();
   var url = document.URL;
   url += ".json"
-  // console.log(url);
     $.ajax({
     url: url,
     dataType: "json",
     type: "GET",
     success: function( response ) {
-      // console.log( response[0] )
       var p = $(".percentage").text();
       if ( p === "100"){
         $('.go').text("Start Quiz");
@@ -24,13 +22,11 @@ $('.go').on('click', function(){
         showQuestion(response);
       }
     },
-    error: function( response) {
-      // console.log("oh no");
+    error: function( response ) {
+      alert("OoH nOo!")
     }
-  })
-})
-
-
+  });
+});
 
 function showQuestion(response) {
   var num = ((Math.random() * response.length));
@@ -39,28 +35,21 @@ function showQuestion(response) {
   var a = (response[num]["answer"]);
   console.log(a);
   render(q, a);
-}
-
-
-
+};
 
 $(".guess-btn").on('click', function(){
   var guess = $(".guess").val();
   var answer = $("#hidden").text();
-  answer = answer.replace(/\s/g, '');
-  answer = answer.replace(/\W/g, '')
-  guess = guess.replace(/\s/g, '');
-  guess = guess.replace(/\W/g, '')
+  answer = answer.replace(/\W/g, '');
+  guess = guess.replace(/\W/g, '');
   answer = answer.toLowerCase();
   guess = guess.toLowerCase();
-  // console.log(guess)
-  // console.log(answer)
   checkAnswer(guess, answer);
   $('.go').toggle();
   $('.custom-go').toggle();
   $(this).toggle();
   $('#hidden').show();
-})
+});
 
 function checkAnswer(a,b) {
   var lengthA = a.length;
@@ -72,14 +61,12 @@ function checkAnswer(a,b) {
       if(a[i] == b[i]) {
           equivalency++;
       }
-  }
+  };
   var weight = equivalency / maxLength;
-  // console.log((weight * 100) + "%");
   weight = (weight * 100);
   if (weight > 60) {
     var url = document.URL;
     url += ".json"
-    // console.log(url);
       $.ajax({
       url: url,
       dataType: "json",
@@ -93,13 +80,10 @@ function checkAnswer(a,b) {
         else {
           meterScore(leng);
         }
-        // return meterScore(response.length);
-        }
-      });
-
-  }
-}
-
+      }
+    });
+  };
+};
 
 var paths = document.getElementsByClassName('meter');
 var path = paths[paths.length-1];
@@ -108,42 +92,35 @@ $(path).css({
   'stroke-dasharray': 500,
   'stroke-dashoffset': 0,
   'opacity': 1,
-})
+});
 
 function runner(percentage, fill){
   setTimeout( function() {
-    $('.percentage').text(percentage)
+    $('.percentage').text(percentage);
     $(path).css('stroke-dashoffset', fill);
   }, (Math.random() * 200));
-}
-
+};
 
 var count = 0;
 var filler = 0;
 
-function meterScore(total){
+function meterScore(total) {
   var size = 377;
-  // console.log(url);
-    // getLength();
-  console.log(total)
+  console.log(total);
   if (count === total) {
     console.log("complete");
   }
   else {
     count++
     var percentage = Math.ceil(100*count/total);
-    // console.log(percentage);
-    filler -= (size/total)
-    // console.log(filler);
+    filler -= (size/total);
     runner(percentage, filler);
-    // console.log(count);
   }
-}
+};
 
 function getLength() {
-var url = document.URL;
-url += ".json"
-// console.log(url);
+  var url = document.URL;
+  url += ".json"
   $.ajax({
   url: url,
   dataType: "json",
@@ -153,12 +130,10 @@ url += ".json"
     // return meterScore(response.length);
     }
   });
-}
-
+};
 
 var foo = 0
-// console.log(foo)
-$('.custom-go').on('click', function(){
+$('.custom-go').on('click', function() {
   $("#state").empty();
   $('.guess-btn').toggle();
   $('.custom-go').hide();
@@ -171,49 +146,48 @@ $('.custom-go').on('click', function(){
     type: "GET",
     success: function( response ) {
       // console.log(response)
-      var p = $(".percentage").text()
+      var p = $(".percentage").text();
       if ( p === "100") {
-        $('.custom-go').text("Start Quiz");
-        $('.custom-go').toggle();
-        $('.guess-btn').toggle();
+        customToggle();
         runner(0,0);
       }
-      else if ( response.length == 0){
-        alert("There are no questions in this quiz! Sorry")
+      else if ( response.length == 0) {
+        alert("There are no questions in this quiz! Sorry");
       }
       else if (foo < response.length ) {
         $('.custom-go').text("Next Question");
         showCustomQuestion(response, foo);
-      foo++
-      // console.log(foo);
+        foo++
       }
       else {
-
-        $('.custom-go').text("Start Quiz");
-        $('.custom-go').toggle();
-        $('.guess-btn').toggle();
+        customToggle();
         runner("",0);
-        foo = 0
-        return foo
+        foo = 0;
+        return foo;
       }
     },
     error: function( response) {
-      // console.log("oh no")
+      console.log("oh no");
     }
-  })
-})
+  });
+});
+
+
+function customToggle() {
+  $('.custom-go').text("Start Quiz");
+  $('.custom-go').toggle();
+  $('.guess-btn').toggle();
+}
 
 function showCustomQuestion(response, foo) {
-  // console.log(foo)
-  // console.log(response[foo].question);
+  $('.guess').val("");
   console.log(response[foo].answer);
   var q = response[foo].question;
   var a = response[foo].answer;
-  render(q, a)
-
-}
+  render(q, a);
+};
 
 function render(question, answer) {
   $("#state").append("<h2>"+ question +"</h2>");
   $("#state").append("<h3 id='hidden'>"+ answer +"</h3>");
-}
+};
